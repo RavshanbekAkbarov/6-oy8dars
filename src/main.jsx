d0e03ef713106e20fc57f89df35bfa3946ef3bee
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2024-12-12 11:29:11 +0500

    tugadi

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { GlobalContextProvider } from "./context/GlobalContext.jsx";
+import { GlobalContextProvider } from "./context/globalContext.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
